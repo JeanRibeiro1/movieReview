9c8f5b3db9aebfbc5b38cb281281a8a0bd880791
jean pereira <jeanpereira15@gmail.com>
2023-07-25 09:18:54 -0300

    created entity movie and the movie controller

---
+++ src/main/java/dev/jean/movies/MovieController.java
@@ -0,0 +1,14 @@
+package dev.jean.movies;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public String allmovies(){
+        return "All movies";
+    }
+}
